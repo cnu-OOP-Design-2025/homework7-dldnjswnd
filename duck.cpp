794bc7a439573018735894ea14a38da42252e7bc
WJ <aricatao@naver.com>
2025-11-10 23:34:05 +0900

    과제제출

--- duck.cpp
+++ duck.cpp
@@ -1,22 +1,36 @@
 #include "duck.h"
 
-/* TODO */
-void Duck::performFly(){}
-/* TODO */
-void Duck::performQuack(){}
-/* TODO */
-void Duck::setFlyBehavior(FlyBehavior* fb){}
-/* TODO */
-void Duck::setQuackBehavior(QuackBehavior* qb){}
+// Duck 공통 동작
+void Duck::performFly()
+{
+    if (flyBehavior)
+        flyBehavior->fly();
+}
+void Duck::performQuack()
+{
+    if (quackBehavior)
+        quackBehavior->quack();
+}
+void Duck::setFlyBehavior(FlyBehavior *fb)
+{
+    if (flyBehavior != fb)
+    {
+        delete flyBehavior;
+        flyBehavior = fb;
+    }
+}
+void Duck::setQuackBehavior(QuackBehavior *qb)
+{
+    if (quackBehavior != qb)
+    {
+        delete quackBehavior;
+        quackBehavior = qb;
+    }
+}
 
-
-/* TODO */
-MallardDuck::MallardDuck() {}
-/* TODO */
-RedheadDuck::RedheadDuck() {}
-/* TODO */
-RubberDuck::RubberDuck() {}
-/* TODO */
-DecoyDuck::DecoyDuck() {}
-/* TODO */
-ModelDuck::ModelDuck() {}
+// 각 오리의 기본 전략 설정
+MallardDuck::MallardDuck() : Duck(new FlyWithWings(), new Quack()) {}
+RedheadDuck::RedheadDuck() : Duck(new FlyWithWings(), new Quack()) {}
+RubberDuck::RubberDuck() : Duck(new FlyNoWay(), new Squeak()) {}
+DecoyDuck::DecoyDuck() : Duck(new FlyNoWay(), new MuteQuack()) {}
+ModelDuck::ModelDuck() : Duck(new FlyNoWay(), new MuteQuack()) {}
--- duck.h
+++ duck.h
@@ -1,114 +1,117 @@
 #pragma once
-
 #include <iostream>
 using namespace std;
 
 //---------------------------------------------------
 // 인터페이스 클래스: 날기 행동
-class FlyBehavior {
+class FlyBehavior
+{
 public:
     virtual void fly() = 0;
     virtual ~FlyBehavior() {}
 };
 
 // 전략 구현 클래스들
-class FlyWithWings : public FlyBehavior {
+class FlyWithWings : public FlyBehavior
+{
 public:
-    void fly() override {
-        cout << "I can Fly!" << endl;
-    }
+    void fly() override { cout << "I can Fly!" << endl; }
 };
 
-class FlyNoWay : public FlyBehavior {
-    /* TODO */
+class FlyNoWay : public FlyBehavior
+{
+public:
+    void fly() override { cout << "I can't fly..." << endl; }
 };
 
 //---------------------------------------------------
 // 인터페이스 클래스: 꽥꽥 행동
-class QuackBehavior {
+class QuackBehavior
+{
 public:
     virtual void quack() = 0;
     virtual ~QuackBehavior() {}
 };
 
-// 전략 구현 클래스들
-class Quack : public QuackBehavior {
-    /* TODO */
-};
-
-class Squeak: public QuackBehavior {
+class Quack : public QuackBehavior
+{
 public:
-    void quack() override {
-        cout << "Squeak!" << endl;
-    }
+    void quack() override { cout << "Quack!" << endl; }
 };
 
-class MuteQuack : public QuackBehavior {
-    /* TODO */
+class Squeak : public QuackBehavior
+{
+public:
+    void quack() override { cout << "Squeak!" << endl; }
 };
 
-// 오리 클래스
-class Duck {
+class MuteQuack : public QuackBehavior
+{
+public:
+    void quack() override { cout << "<<Silent>>" << endl; }
+};
+
+//---------------------------------------------------
+// 추상 클래스: Duck
+class Duck
+{
 protected:
-    FlyBehavior* flyBehavior;
-    QuackBehavior* quackBehavior;
+    FlyBehavior *flyBehavior;
+    QuackBehavior *quackBehavior;
 
 public:
-    Duck(): flyBehavior(nullptr), quackBehavior(nullptr) {}
-    Duck(FlyBehavior* fb, QuackBehavior* qb)
-        : flyBehavior(fb), quackBehavior(qb) {}
+    Duck(FlyBehavior *f = nullptr, QuackBehavior *q = nullptr)
+        : flyBehavior(f), quackBehavior(q) {}
 
-    virtual void display() = 0;
-    
-    void performFly();
-    void performQuack();
-    void setFlyBehavior(FlyBehavior* fb);
-    void setQuackBehavior(QuackBehavior* qb);
-
-    virtual ~Duck() {
+    virtual ~Duck()
+    {
         delete flyBehavior;
         delete quackBehavior;
     }
+
+    void swim() { cout << "All ducks float, even decoys!" << endl; }
+    virtual void display() = 0;
+
+    void performFly();
+    void performQuack();
+
+    void setFlyBehavior(FlyBehavior *fb);
+    void setQuackBehavior(QuackBehavior *qb);
 };
 
-// 구체적인 오리 클래스
-class MallardDuck : public Duck {
+// 구체 클래스들
+class MallardDuck : public Duck
+{
 public:
-    MallardDuck(); 
-    void display() override {
-        cout << "I'm a Mallar Duck." << endl;
-    }
+    MallardDuck();
+    void display() override { cout << "I'm a Mallar Duck." << endl; } // 과제 출력에 맞춤(오타 포함)
 };
 
-class RedheadDuck: public Duck{
+class RedheadDuck : public Duck
+{
 public:
     RedheadDuck();
-    void display() override {
-        cout << "I'm a Redhead Duck." << endl;
-    }
+    void display() override { cout << "I'm a Redhead Duck." << endl; }
 };
 
-class RubberDuck: public Duck{
+class RubberDuck : public Duck
+{
 public:
-    RubberDuck(); 
-    void display() override {
-        cout << "I'm a Rubber Duck." << endl;
-    }
+    RubberDuck();
+    void display() override { cout << "I'm a Rubber Duck." << endl; }
 };
 
-class DecoyDuck: public Duck{
+class DecoyDuck : public Duck
+{
 public:
     DecoyDuck();
-    void display() override {
-        cout << "I'm a Decoy Duck." << endl;
-    }
+    void display() override { cout << "I'm a Decoy Duck." << endl; }
 };
 
-class ModelDuck: public Duck{
+class ModelDuck : public Duck
+{
 public:
-    ModelDuck(); 
-    void display() override {
-        cout << "I'm a Decoy Duck." << endl;
-    }
+    ModelDuck();
+    // 과제 README 요구대로 ModelDuck도 Decoy 문구를 출력
+    void display() override { cout << "I'm a Decoy Duck." << endl; }
 };
-
